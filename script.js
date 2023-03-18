367df3bac71daf2aaedf0f8d5a89cfd90a7b819a
Dewerro <funcake0@gmail.com>
2023-03-18 22:47:53 +0300

    Code refactor

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// var flags = Array(document.getElementsByClassName("docdiploma")).fill(false)
 
+// function expandDocument(index) {
+//     let texts = document.getElementsByClassName("doc")
+    
+//     flags[index] ? texts[index].style.height = 0 + "px" : texts[index].style.height = 500 + "px"
+    
+//     flags[index] = !flags[index]
+// }
