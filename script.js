d464a8dba57d6b6ed675efc53691395d48549bec
Dewerro <funcake0@gmail.com>
2023-04-18 22:41:04 +0300

    Fix document sizes

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-setDropdownWidth()
-
 const animationFlags = Array(document.getElementsByClassName("dropdown")).fill(false)
 
 function makeAnimation(index) {
@@ -15,9 +13,11 @@ function makeAnimation(index) {
 }
 
 const pdfDocuments = document.getElementsByClassName("document")
+const dropdownTitle = document.getElementsByClassName("dropdown__title")
 
 function changeDocumentSize(index, isExpanded) {
     const doc = pdfDocuments[index]
+    const title = dropdownTitle[index]
 
     const defaultSizeValue = 0 + "px"
     const expandSize = getExpandSize()
@@ -27,11 +27,15 @@ function changeDocumentSize(index, isExpanded) {
     if (isExpanded) {
         doc.style.height = defaultSizeValue
         doc.style.width = defaultSizeValue
-        doc.style.marginBottom = defaultSizeValue;
+        doc.style.marginBottom = defaultSizeValue
+        title.style.marginBottom = defaultSizeValue
     } else {
         doc.style.height = expandHeight
         doc.style.width = expandWidth
-        doc.style.marginBottom = 20 + "px";
+        doc.style.marginLeft = 20 + "px"
+        doc.style.marginRight = 20 + "px"
+        doc.style.marginBottom = 20 + "px"
+        title.style.marginBottom = 10 + "px"
     }
 }
 
@@ -47,30 +51,16 @@ function getExpandSize() {
     const width = document.body.clientWidth
 
     if (width < 321) {
-        return { width: 300, height: 300 }
+        return { height: 300 }
     } else if (width < 480) {
-        return { width: 360, height: 300 }
+        return { height: 350 }
     } else if (width < 768) {
-        return { width: 400, height: 300 }
+        return { height: 500 }
+    } else if (width < 1025) {
+        return { width: 640, height: 500 }
     } else if (width < 1200) {
-        return { width: 500, height: 400 }
-    } else if (width < 1440) {
-        return { width: 600, height: 500 }
+        return { width: 940, height: 400 }
     } else {
-        return { width: 700, height: 600 }
+        return { width: 1000, height: 600 }
     }
-}
-
-function setDropdownWidth() {
-    const dropdowns = document.getElementsByClassName("dropdown")
-    const lines = document.getElementsByTagName("hr")
-
-    const expandSize = getExpandSize()
-    const hrMargin = 70
-
-    for (let dropdown of dropdowns)
-        dropdown.style.width = expandSize.width + "px"
-
-    for (let line of lines)
-        line.style.width = expandSize.width - hrMargin + "px"
 }
